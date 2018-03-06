bb0ed8351aebf099f6a31fa3df752c30114404e4
Issan0 <Student@193.107.34.218>
2018-03-06 12:16:14 +0100

    NIE DZIALA XDDDDDDDDDD

--- blog2018/src/blog2018/Blog.java
+++ blog2018/src/blog2018/Blog.java
@@ -1,4 +1,7 @@
 package blog2018;
+import java.time.LocalDate;
+import static java.time.LocalDate.*;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 public class Blog {
     private List<Note> notes = new ArrayList();
@@ -14,4 +17,44 @@ public class Blog {
     {
         return title+" \n\n"+notes;
     }
+
+public Note mostPopular()
+{
+    if(notes.isEmpty()) return null;
+    Note result = notes.get(0);
+    for(Note note: notes){
+       int count = note.numberOfComments();
+       if(count>result.numberOfComments()){
+           result = note;
+       }
+    }
+return result;
+}
+public List<Note> mostPopulars()
+{
+return null;
+}
+public Comment lastComment()
+{
+    List<Comment> allComments = new.ArrayList();
+    for(Note note : notes)
+    {
+        allComments.addAll(note.getComments());
+    }
+    Comment result = allComments.get(0);
+    for(Comment comment : allComments)
+    {
+        LocalDate d1=comment.getDate();
+         LocalDate d2=comment.getDate();
+         if(d1.isAfter(d2))
+         {
+             result=comment;
+         }
+    }
+return null;
+}
+public List<Comment> lastComments()
+{
+return null;
+}
 }
--- blog2018/src/blog2018/Note.java
+++ blog2018/src/blog2018/Note.java
@@ -1,4 +1,7 @@
 package blog2018;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 public class Note {
     private String title, content;
@@ -16,25 +19,39 @@ public class Note {
         StringBuilder sb = new StringBuilder();
         sb.append("Wpis\n");
         sb.append("tytul: "+title);
-        sb.append("tresc: "+content);
-        sb.append("komentarze: "+comments);
+        sb.append("  tresc: "+content);
+        sb.append("  komentarze: "+comments);
         sb.append("/n");
         return sb.toString();
     }
     
     public static void main(String[] args) {
-        Blog blog = new Blog("Blog programistyczny");
-        Note note1 = new Note("co nowego - java 9","co nowego - java 9");
-        Note note2 = new Note("Kolekcje","kolekcje");
-        Note note3 = new Note("JavaFX", "JavaFX");
+        Blog blog = new Blog("  Blog programistyczny");
+        Note note1 = new Note("  co nowego - java 9","  co nowego - java 9");
+        Note note2 = new Note("  Kolekcje","  kolekcje");
+        Note note3 = new Note("  JavaFX", "  JavaFX");
         blog.add(note3);
         blog.add(note2);
         blog.add(note1);
-        Comment c1 = new Comment("Rafal","Kolekcje sa trudne");
+        Comment c1 = new Comment("  Rafal","  Kolekcje sa trudne");
         note2.add(c1);
-        Comment c2 = new Comment("Krzysztof","Kolekcje sa latwe");
+        Comment c2 = new Comment("  Krzysztof","  Kolekcje sa latwe");
         note2.add(c2);
-        
+        System.out.println(blog);
+        System.out.println("-----------");
+        System.out.println(blog.mostPopular());
+          System.out.println("-----------");
+          LocalDate d1= LocalDate.of(2018,Month.MARCH,1);
+          LocalDate d2= LocalDate.of(2018,Month.FEBRUARY,2);
+          long u = d1.until(d2,ChronoUnit.DAYS);
+          System.out.println(u);
+           long u2 = d2.until(d1,ChronoUnit.DAYS);
+          System.out.println(u2);
+         System.out.println("-----------");
+         System.out.println(blog.lastComment())
+          
     }
-    
+    public int numberOfComments(){
+        return comments.size();
+    }
 }
